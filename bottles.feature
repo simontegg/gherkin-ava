c0e847d86ab1017fd880bb1840082e610361634c
simontegg <simon@enspiral.com>
2017-05-07 01:14:16 +1200

    update-ast

--- bottles.feature
+++ bottles.feature
@@ -4,4 +4,4 @@ Scenario: A bottle falls from the wall
 
     Given 100 green bottles are standing on the wall
     When 1 green bottle accidentally falls
-    When there are 99 green bottles standing on the wall
+    Then there are 99 green bottles standing on the wall
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const acorn = require('acorn')
 const fs = require('fs')
+const isArray = require('is-array')
 const pretty = require('prettyjson')
 const escodegen = require('escodegen')
 const traverse = require('traverse')
@@ -40,6 +41,8 @@ stream.on('data', data => {
       library[keyword].push({ re: RegExp(arg0.value, 'g'), node: arg1 })
     }
   })
+
+  console.log(library)
   
   const featureFileStream = fs.createReadStream(__dirname + '/bottles.feature', 'utf8')
   featureFileStream.on('data', file => {
@@ -47,92 +50,105 @@ stream.on('data', data => {
     const { name, children } = parser.parse(scanner).feature
     featureDeclaration = getFeatureDeclaration(name)
     const scenarioDeclarations = []
-    const scenarios = []
 
-    // scenarios
-    children.forEach(function ({ name, steps }, i) {
+    const scenarios = children.map(function ({ name, steps }, i) {
       scenarioDeclarations.push(getScenarioDeclaration(name))
-      scenarios.push([])
 
-      steps.forEach(function (step) {
+      return steps.reduce(function (acc, step, i, steps) {
         const keyword = step.keyword.trim()
         const { text } = step
 
         if (library[keyword]) {
           // assume 1 match
           const match = library[keyword].find(libraryStep => libraryStep.re.test(text))
+          if (!match) throw new Error('no matching step')
 
-          if (match) {
-            const params = match.node.params.map(param => param.name)
-            let isAsync = false
+          const params = match.node.params.map(param => param.name)
+          let isAsync = false
 
-            // synchronous no params
-            if (params.length === 0) {
-              scenarios[i].push(getBody(match.node.body)) 
-            }
-
-            // evaluate
-            if (params.length > 0) {
-              console.log(params)
-              match.re.lastIndex = 0 // reset regex
-              const vars = match.re.exec(text).slice(1).map(v => {
-                const coerced = v.includes('.') ? parseFloat(v) : parseInt(v, 10)
-                if (!isNaN(coerced)) return coerced
-                return v
-              })
-
-              // replace params with actual value
-              traverse(match.node.body).forEach(function (x) {
-                const index = this.node ? params.indexOf(this.node.name) : -1
-
-                if (index > -1) {
-                  console.log(vars[index])
-                  this.update({
-                   type: 'Literal',
-                   value: vars[index]
-                  })
-                }
-
-                if (this.node && this.node.name === 'next') isAsync = true
-              })
-
-              const body = getBody(match.node.body)
-
-              if (isAsync) {
-                const parts = body.split('next()') 
-                const generateAsync = next => {
-                  parts.splice(1, 0, next)
-                  return parts.slice(0).join('')
-                } 
-                scenarios[i].push(generateAsync)   
-              } else {
-                scenarios[i].push(body) 
-              }
-
-              console.log(isAsync)
-              
-            }
-          } else {
-            // error no matching library step
+          // synchronous, no params
+          if (params.length === 0) {
+            acc.push(match.node.body)
+            return acc
           }
 
+          // evaluate
+          if (params.length > 0) {
+            console.log(params)
+            match.re.lastIndex = 0 // reset regex
+            const vars = getVariables(match.re, text)
 
+            // replace params with actual value
+            traverse(match.node.body).forEach(function (x) {
+              const index = this.node ? params.indexOf(this.node.name) : -1
+
+              if (index > -1) {
+                this.update({
+                  type: 'Literal',
+                  value: vars[index]
+                })
+              }
+
+              if (this.node && this.node.name === 'next') isAsync = true
+            })
+
+            console.log('body', pretty.render(match.node.body))
+
+            acc.push({ node: match.node.body, isAsync })
+            return acc
+          }
         }
+     }, [])
 
-        
-      })
-      
-    })
+   })
+
+    console.log(scenarios)
+
+
+  const body = scenarios.map(function (scenario) {
+   const steps = scenario.reverse().reduce(function (body, step, i, steps) {
+    const nextStep = steps[i + 1]
+
+    if (nextStep && nextStep.isAsync) {
+      recurseStep(nextStep, step)
+      return body
+    }
+
+    body.unshift(step.node.body)
+    return body
+   }, [])
+
+    return steps
+  })
+
   
-    
-  
+  const ast = { type: 'BlockStatement', body }
+
+  console.log('output', pretty.render(body))
+  body[0].forEach(function (step, i) {
+    console.log(step)
+    console.log(escodegen.generate(step[0], escodegenOptions))
+
   })
 
 
+  
 
 
 
 })
+
+})
+
+function recurseStep (nextStep, step) {
+  console.log(step.node)
+  traverse(nextStep).forEach(function (x) {
+    if (x && x.name === 'next') {
+     console.log(x)
+     this.parent.update(step.node)
+    }
+  })
+}
 
 function getFeatureDeclaration (name) {
   return function (body) {
@@ -148,6 +164,14 @@ function getScenarioDeclaration (name) {
 
 function getBody (node) {
   return escodegen.generate(node, escodegenOptions).replace(/^{|}$/g, '')
+}
+
+function getVariables(regex, text) {
+  return regex.exec(text).slice(1).map(v => {
+    const coerced = v.includes('.') ? parseFloat(v) : parseInt(v, 10)
+    if (!isNaN(coerced)) return coerced
+    return v
+  })
 }
 
 
