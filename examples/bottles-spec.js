1ba08414d81b59fa2b36f4d90e7318386a8d5fb2
simontegg <simon@enspiral.com>
2017-05-07 22:46:29 +1200

    async-ready

--- examples/bottles-spec.js
+++ examples/bottles-spec.js
@@ -1,14 +1,12 @@
-const {feature} = require('ava-spec');
-const Wall = require('./wall');
+const { feature } = require('ava-spec')
+const Wall = require('./wall')
 feature('Ava Asynchronous Example', scenario => {
-    scenario.cb('A bottle falls from the wall', t => {
-        const wall = new Wall(num);
-        asyncFuntion(num, err => {
-            if (err)
-                t.end(err);
-            wall.fall(num);
-            next();
-        });
-        t.is(num, wall.items);
-    });
-});
+  scenario.cb('A bottle falls from the wall', t => {
+    const wall = new Wall(100)
+    asyncFuntion(1, err => {
+      if (err) t.end(err)
+      wall.fall(1)
+      t.is(99, wall.items)
+    })
+  })
+})
